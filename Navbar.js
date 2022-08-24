35338daa1fbc301b5ff4344d759f4934829a1b3c
Davanjit Sandhu <davanjit@hotmail.com>
2022-08-24 14:33:21 -0400

    Update Navbar.js

--- Navbar.js
+++ Navbar.js
@@ -1,15 +1,12 @@
 import './Navbar.css'
 
 const Navbar = ({title, items}) => {
-
-  let key = 0
-
   return(
     <nav className="navbar">
       <span className="title">{title}</span>
       <span className="items">
         {items.map(i => 
-          <span key={key++} className="item" onClick={i.clickEvent} >{i.name}</span>
+          <span key={i.id} className="item" onClick={i.clickEvent} >{i.name}</span>
         )}
       </span>
     </nav>
